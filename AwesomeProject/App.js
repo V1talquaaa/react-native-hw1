29dcdd2ccdd2975d38e7c74a368d15cbf1fec8dd
V1talquaaa <115567744+V1talquaaa@users.noreply.github.com>
2023-06-30 13:29:40 +0200

    Update App.js

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text >Hello world!</Text>
-      <Text>I`m first project</Text>
+      <Text>I`m first homework</Text>
       <StatusBar style="auto" />
     </View>
   );
